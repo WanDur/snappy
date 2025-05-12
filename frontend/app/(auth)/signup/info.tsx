d76fcea91aa6d56b86f4afbc9c487668ad1b7ecc
Clement Ching <lhemon412@outlook.com>
2025-05-13 07:18:15 +0800

    feat: signup page

--- frontend/app/(auth)/signup/info.tsx
+++ frontend/app/(auth)/signup/info.tsx
@@ -1,51 +1,49 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Alert } from 'react-native'
 import { useState } from 'react'
 import { useRouter, useLocalSearchParams } from 'expo-router'
 import RNPickerSelect from 'react-native-picker-select'
+import CountryPicker, { CountryCode, DARK_THEME, DEFAULT_THEME } from 'react-native-country-picker-modal'
 
 import { Themed, Bounceable } from '@/components'
 import { Stack } from '@/components/router-form'
 import { useTheme } from '@/hooks'
+import { useSession } from '@/contexts/auth'
 
 const SignUpInfo = () => {
   const { username, email, password } = useLocalSearchParams<{ username: string; email: string; password: string }>()
   const { colors, isDark } = useTheme()
+  const { signUpWithCredential } = useSession()
   const router = useRouter()
+  
+  const [displayName, setDisplayName] = useState('')
+  const [countryCode, setCountryCode] = useState<CountryCode>('HK')
+  const [callingCode, setCallingCode] = useState<string>('+852')
+  const [phoneNumber, setPhoneNumber] = useState<string>('')
 
-  const [firstName, setFirstName] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [honorific, setHonorific] = useState('')
-
-  const canNext = firstName.length > 0 && lastName.length > 0 && honorific !== ''
+  const canNext = displayName.length > 0
 
   const onSignUp = async () => {
     if (!canNext) return
-    console.log('Signing up with', username, email, password, firstName, lastName, honorific)
-    router.dismissTo('/(auth)/LoginScreen')
-    // TODO send request to server and confirm result
-    /*
+    console.log('Signing up with', username, email, password, displayName)
+    
     try {
       await signUpWithCredential(
         {
           username,
           email,
           password,
-          name: `${firstName} ${lastName}`
+          name: displayName,
+          phone: `(${callingCode}) ${phoneNumber}`
         },
-        {
-          firstName,
-          lastName,
-          honorific
-        },
-        settings.userType!
       )
-      router.back()
-      Alert.alert('Success', 'Please check your email to verify')
+      router.dismissTo('/(auth)/LoginScreen')
+      Alert.alert('Success', 'You may login to your account now')
     } catch (err) {
       if (err instanceof Error) {
         Alert.alert('Error', err.message)
+        router.dismissTo('/(auth)/LoginScreen')
       }
-    }*/
+    }
   }
 
   return (
@@ -53,47 +51,53 @@ const SignUpInfo = () => {
       <Stack.Screen options={{ title: 'Signup Info' }} />
       <Themed.ScrollView style={{ flex: 1, padding: 16 }} keyboardShouldPersistTaps="handled">
         <Themed.TextInput
-          label="First Name"
-          placeholder="First Name"
+          label="Display Name"
+          placeholder="Display Name"
           autoCapitalize="words"
-          onChangeText={setFirstName}
-          error={firstName.length === 0 ? 'Required' : ''}
-          value={firstName}
+          onChangeText={setDisplayName}
+          error={displayName.length === 0 ? 'Required' : ''}
+          value={displayName}
           autoFocus
         />
-        <Themed.TextInput
-          label="Last Name"
-          placeholder="Last Name"
-          autoCapitalize="words"
-          error={lastName.length === 0 ? 'Required' : ''}
-          onChangeText={setLastName}
-          value={lastName}
-        />
-        <Themed.Text style={styles.label}>Honorific</Themed.Text>
-        <RNPickerSelect
+        <Themed.Text style={styles.label}>Phone</Themed.Text>
+        <View
           style={{
-            viewContainer: {
-              borderRadius: 12,
-              height: 50,
-              padding: 14,
-              backgroundColor: isDark ? '#2d2d37' : 'rgb(229, 229, 234)',
-              marginBottom: 16,
-              justifyContent: 'center'
-            },
-            inputIOSContainer: {
-              pointerEvents: 'none',
-              height: 50,
-              justifyContent: 'center'
-            },
-            inputIOS: { color: colors.text, fontSize: 16 },
-            placeholder: { color: 'grey' }
+            flexDirection: 'row',
+            alignItems: 'center',
+            backgroundColor: isDark ? '#2d2d37' : 'rgb(229, 229, 234)',
+            minHeight: 50,
+            padding: 8,
+            borderRadius: 12,
+            marginBottom: 16,
           }}
-          value={honorific}
-          onValueChange={setHonorific}
-          placeholder={{ label: 'Honorific', value: null, color: 'grey' }}
-          items={['mr', 'ms', 'mrs', 'dr'].map((hon) => ({ label: `honorific.${hon}`, value: hon }))}
-          darkTheme={isDark}
-        />
+        >
+          <View style={{ justifyContent: 'center' }}>
+            <CountryPicker
+              countryCode={countryCode}
+              onSelect={(country) => {
+                setCountryCode(country.cca2)
+                setCallingCode(country.callingCode[0])
+                console.log(country.callingCode)
+              }}
+              containerButtonStyle={{
+                marginHorizontal: 16,
+              }}
+              theme={isDark ? DARK_THEME : DEFAULT_THEME}
+              withCallingCode
+              withCallingCodeButton
+              withFlagButton
+              preferredCountries={["HK", "MO", "CN", "TW"]}
+            />
+          </View>
+          <Themed.TextInput
+            placeholder="Phone Number"
+            onChangeText={setPhoneNumber}
+            value={phoneNumber}
+            keyboardType="phone-pad"
+            containerStyle={{ flexGrow: 1}}
+          />
+        </View>
+        
         <Bounceable
           style={{
             backgroundColor: canNext ? '#007AFF' : colors.secondaryBg,
